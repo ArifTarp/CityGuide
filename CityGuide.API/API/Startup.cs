53243b64586524c8c3a9af074bc1d865355e5193
ArifTarp <arifoklh@gmail.com>
2021-02-20 18:34:48 +0300

    Changed lifetime strategy

--- CityGuide.API/API/Startup.cs
+++ CityGuide.API/API/Startup.cs
@@ -53,7 +53,7 @@ namespace API
                 });
             });
 
-            services.AddSingleton<IAppRepository, AppRepository>();
+            services.AddScoped<IAppRepository, AppRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
