ab0a25eba379df5086d1f562dc6bc75c2561f601
ArifTarp <arifoklh@gmail.com>
2021-02-20 16:56:14 +0300

    Changed lines

--- CityGuide.API/API/Models/City.cs
+++ CityGuide.API/API/Models/City.cs
@@ -13,12 +13,12 @@ namespace API.Models
         }
 
         public int Id { get; set; }
-        public string Description { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+
+
         public int UserId { get; set; }
-
-
-        public List<Photo> Photos { get; set; }
         public User User { get; set; }
+        public List<Photo> Photos { get; set; }
     }
 }
--- CityGuide.API/API/Models/User.cs
+++ CityGuide.API/API/Models/User.cs
@@ -13,9 +13,9 @@ namespace API.Models
         }
 
         public int Id { get; set; }
+        public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public string UserName { get; set; }
 
 
         public List<City> Cities { get; set; }
