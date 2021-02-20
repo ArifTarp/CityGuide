ea1353c3818529c4546a62e6699ec7250c84e551
ArifTarp <arifoklh@gmail.com>
2021-02-20 16:50:55 +0300

    Added API

---
+++ CityGuide.API/API/Data/AuthRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private DataContext _dataContext;
+        public AuthRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _dataContext.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<User> Login(string userName, string password)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] userPasswordHash, byte[] userPasswordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(userPasswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != userPasswordHash[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public async Task<bool> UserExists(string userName)
+        {
+            if (await _dataContext.Users.AnyAsync(u=>u.UserName==userName))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ CityGuide.API/API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
