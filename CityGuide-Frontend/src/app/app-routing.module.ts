a57e2579545526e3ea787589f009cb3ec4be344f
ArifTarp <arifoklh@gmail.com>
2021-02-24 16:41:30 +0300

    Added frontend operations

--- CityGuide-Frontend/src/app/app-routing.module.ts
+++ CityGuide-Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CityDetailComponent } from "./city/cityDetail/cityDetail.component";
+import { CityAddComponent } from './city/cityAdd/cityAdd.component';
+import { RegisterComponent } from './register/register.component';
+import { CityComponent } from "./city/city.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "city", component: CityComponent },
+  { path: "cityDetail/:cityId", component: CityDetailComponent },
+  { path: "cityAdd", component: CityAddComponent},
+  { path: "register", component: RegisterComponent},
+  { path: "**", redirectTo: "city", pathMatch: "full" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule ]
 })
 export class AppRoutingModule { }
--- CityGuide-Frontend/src/app/app.module.ts
+++ CityGuide-Frontend/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgxEditorModule } from 'ngx-editor';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
+import { CityDetailComponent } from './city/cityDetail/cityDetail.component';
+import { CityAddComponent } from './city/cityAdd/cityAdd.component';
+import { RegisterComponent } from './register/register.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CityComponent,
+    CityDetailComponent,
+    CityAddComponent,
+    RegisterComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxGalleryModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    NgxEditorModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
