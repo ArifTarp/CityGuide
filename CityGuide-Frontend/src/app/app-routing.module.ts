e7bbf2b5cccd04637fb9b296999d5de970a70b36
ArifTarp <arifoklh@gmail.com>
2021-02-24 17:45:49 +0300

    Added authguard

--- CityGuide-Frontend/src/app/app-routing.module.ts
+++ CityGuide-Frontend/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { CityDetailComponent } from "./city/cityDetail/cityDetail.component";
 import { CityAddComponent } from './city/cityAdd/cityAdd.component';
 import { RegisterComponent } from './register/register.component';
 import { CityComponent } from "./city/city.component";
+import { AuthGuardService } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: "city", component: CityComponent },
+  { path: "cities", component: CityComponent },
   { path: "cityDetail/:cityId", component: CityDetailComponent },
-  { path: "cityAdd", component: CityAddComponent},
+  { path: "cityAdd", component: CityAddComponent, canActivate:[AuthGuardService] },
   { path: "register", component: RegisterComponent},
   { path: "**", redirectTo: "city", pathMatch: "full" }
 ];
